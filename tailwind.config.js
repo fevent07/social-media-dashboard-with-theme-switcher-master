b029af33787b903293879d8f5c33ee1d7eab8f06
fevent07 <feventsige.gg@gmail.com>
2023-08-29 10:06:36 -0400

    structuring for vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ['./src/*.html'],
   theme: {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
